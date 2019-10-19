25122569f8cd3499a3c88c2e8896b228cfc55b4a
username <user@email.mail>
2019-10-19 20:04:58 +0900

    Updates

---
+++ precache-manifest.eb895b381b0589b3484f59519087fd00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ea741aa26e51964540d98a72a4fb53",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9b166fe964120c943dd1",
+    "url": "/my-app/static/css/main.df1e7eea.chunk.css"
+  },
+  {
+    "revision": "fe52cdc74dcb434fc4f9",
+    "url": "/my-app/static/js/2.6a4de570.chunk.js"
+  },
+  {
+    "revision": "9b166fe964120c943dd1",
+    "url": "/my-app/static/js/main.a4076b10.chunk.js"
+  },
+  {
+    "revision": "4883f767f8b6e335f294",
+    "url": "/my-app/static/js/runtime-main.74992542.js"
+  }
+]);
